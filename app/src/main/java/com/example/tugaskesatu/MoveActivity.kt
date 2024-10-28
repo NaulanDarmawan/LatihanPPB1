f429be66d107d7d5d6a741ff483578bbf20b13c3
Naulan Darmawan <naulandarmawan588@gmail.com>
2024-10-28 14:20:37 +0700

    Tugas 2 PPB1 - 28 Oktober 2024

--- app/src/main/java/com/example/tugaskesatu/MoveActivity.kt
+++ app/src/main/java/com/example/tugaskesatu/MoveActivity.kt
@@ -8,24 +8,23 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MoveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_move)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
         val btnMoveActivity = findViewById<Button>(R.id.btn_move_activity)
         btnMoveActivity.setOnClickListener {
             onClick()
         }
     }
     private fun onClick() {
-        val intent = Intent(applicationContext, MoveActivity::class.java)
+        val intent = Intent(applicationContext, MainActivity::class.java)
         startActivity(intent)
     }
 }
